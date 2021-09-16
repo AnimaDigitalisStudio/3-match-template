898e5a90f072b534a65c01fb7b2b0f4190dd6113
AnimaDigitalisStudio <animadigitalis@gmail.com>
2021-09-16 04:32:53 +0200

    poping

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -17,6 +17,7 @@ public sealed class Board : MonoBehaviour
     public int Height => Tiles.GetLength( dimension: 1);
     private void Awake() => Instance = this;
     private readonly List<Tile> _selection = new List<Tile>();
+    private const float TweenDuration = 0.25f;
 
     private void Start()
     {
@@ -38,7 +39,7 @@ public sealed class Board : MonoBehaviour
         }
     }
 
-    public void Select(Tile tile)
+    public async void Select(Tile tile)
     {
         if (!_selection.Contains(tile)) _selection.Add(tile);
 
@@ -46,11 +47,93 @@ public sealed class Board : MonoBehaviour
 
         Debug.Log($"Selected tales at ({_selection[0].x}, {_selection[0].y}) and ({_selection[1].x}, {_selection[1].y})");
 
+        await Swap(_selection[0], _selection[1]);
+
+        if (CanPop())
+        {
+            Pop();
+        }
+        else
+        {
+            await Swap(_selection[0], _selection[1]);
+        }
+
         _selection.Clear();
     }
 
-    public void Swap(Tile tile1, Tile tile2)
+    public async Task Swap(Tile tile1, Tile tile2)
     {
-        
+        var icon1 = tile1.icon;
+        var icon2 = tile2.icon;
+
+        var icon1Transform = icon1.transform;
+        var icon2Transform = icon2.transform;
+
+        var sequence = DOTween.Sequence();
+        sequence.Join(icon1Transform.DOMove(icon2Transform.position, TweenDuration))
+                .Join(icon2Transform.DOMove(icon1Transform.position, TweenDuration));
+
+        await sequence.Play().AsyncWaitForCompletion();
+
+        icon1Transform.SetParent(tile2.transform);
+        icon2Transform.SetParent(tile1.transform);
+
+        tile1.icon = icon2;
+        tile2.icon = icon1;
+
+        var tile1Item = tile1.Item;
+
+        tile1.Item = tile2.Item;
+        tile2.Item = tile1Item;
+    }
+
+    private bool CanPop()
+    {
+        for (var y = 0; y < Height; y++)
+        {
+            for (var x = 0; x < Width; x++)
+            {
+                if (Tiles[x, y].GetConnectedTiles().Skip(1).Count() >= 2)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private async void Pop()
+    {
+        for (var y = 0; y < Height; y++)
+        {
+            for (var x = 0; x < Width; x++)
+            {
+                var tile = Tiles[x, y];
+
+                var connectedTiles = tile.GetConnectedTiles();
+
+                if (connectedTiles.Skip(1).Count() < 2) continue;
+
+                var deflatesequence = DOTween.Sequence();
+
+                foreach (var item in connectedTiles) deflatesequence.Join(item.icon.transform.DOScale(Vector3.zero, TweenDuration));
+                
+                await deflatesequence.Play().AsyncWaitForCompletion();
+
+                var inflateSequence = DOTween.Sequence();
+
+                foreach (var item in connectedTiles) 
+                {
+                    item.Item = ItemDatabase.Items[Random.Range(0, ItemDatabase.Items.Length)];
+
+                    inflateSequence.Join(item.icon.transform.DOScale(Vector3.one, TweenDuration));
+
+                }
+
+                await inflateSequence.Play().AsyncWaitForCompletion();
+
+            }
+        }
     }
 }
